8f3868f0178023d21da71e105cee71c85ed3adb9
AnAsura <carl-campbell-III@yandex.com>
2018-12-02 20:21:13 -0500

    Day2 part 1

--- day_two/src/main.rs
+++ day_two/src/main.rs
@@ -1,11 +1,26 @@
-//count exactly 2 of any letter
-//count  exactly 3 of any letter
-// print the product of 
-use std::collections::HashSet;
 use std::fs;
+use std::collections::HashMap; 
 
 fn main() {
-    let triple_counter = 0_i8;
-    let double_counter = 0_i8;
-}
+    let mut triple_counter = 0_i32;
+    let mut double_counter = 0_i32;
+    let input = fs::read_to_string("input").unwrap();
 
+    for word in input.lines() {
+        let mut hash = HashMap::new();
+        
+        for chars in word.chars() {
+            let count = hash.entry(chars).or_insert(0);
+            *count += 1;
+        }
+
+        if hash.values().any(|&x| x == 2 ) {
+            double_counter += 1;
+        }
+        
+        if hash.values().any(|&x| x == 3) {
+            triple_counter += 1; 
+        }
+    }
+    println!("checksum: {}", triple_counter * double_counter);
+}
