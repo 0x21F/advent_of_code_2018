567083d30010d388f6dc67eade084326a970a372
AnAsura <carl-campbell-III@yandex.com>
2018-12-10 19:54:21 -0500

    Day_two

--- day_two/src/main.rs
+++ day_two/src/main.rs
@@ -1,11 +1,11 @@
 use std::fs;
 use std::collections::HashMap; 
 
+
 fn main() {
     let mut triple_counter = 0_i32;
     let mut double_counter = 0_i32;
     let input = fs::read_to_string("input").unwrap();
-
     for word in input.lines() {
         let mut hash = HashMap::new();
         
@@ -23,4 +23,19 @@ fn main() {
         }
     }
     println!("checksum: {}", triple_counter * double_counter);
+    
+    // part II 
+    for (i, potato) in input.lines().enumerate(){
+        for mut  x in input.lines().skip(i).map(|x| x.to_string()){
+            let actual: Vec<usize> = x.chars().zip(potato.chars())
+                .enumerate()
+                .filter(|(_, (x, y))| x != y)
+                .map(|(x, (_, _))| x )
+                .collect();
+            if actual.len() == 1 {
+                x.remove(actual[0]);
+                println!("{}", x);
+            }
+        }
+    }
 }
